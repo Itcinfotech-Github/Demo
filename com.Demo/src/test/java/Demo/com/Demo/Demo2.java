fbaa1537ed519407e16c2d5fc38d6982efbc6fbb
22706 <22706@BLRPRAGWD22706.ITCINFOTECH.com>
2017-10-26 11:49:37 +0530

    commit the changes

---
+++ com.Demo/src/test/java/Demo/com/Demo/Demo2.java
@@ -0,0 +1,13 @@
+package Demo.com.Demo;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello234");
+		System.out.println("Hello2345");
+
+	}
+
+}
